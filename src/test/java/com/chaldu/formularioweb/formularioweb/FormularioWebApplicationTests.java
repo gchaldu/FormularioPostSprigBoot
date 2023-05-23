a5e95b756b57becd0dc692fdc43db7f5f036b5b2
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-05-23 13:13:59 -0300

    version 1.0 de gestion de formulario methd post

---
+++ src/test/java/com/chaldu/formularioweb/formularioweb/FormularioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaldu.formularioweb.formularioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
