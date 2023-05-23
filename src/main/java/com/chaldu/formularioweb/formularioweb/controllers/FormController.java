7e4c5261af9a6528172a549d26f77c394497d4d0
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-05-23 14:59:55 -0300

    version falta mostrar correctamente mensajes de error en la vista

--- src/main/java/com/chaldu/formularioweb/formularioweb/controllers/FormController.java
+++ src/main/java/com/chaldu/formularioweb/formularioweb/controllers/FormController.java
@@ -1,10 +1,18 @@
 package com.chaldu.formularioweb.formularioweb.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.chaldu.formularioweb.formularioweb.models.domain.Usuario;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class FormController {
@@ -13,18 +21,31 @@ public class FormController {
     public String form(Model model) {
         return "form";
     }
-
     @PostMapping("/form")
+    public String procesarForm(@Valid Usuario usuario,BindingResult result ,Model model) {
+
+        model.addAttribute("titulo", "Resultado del POST");
+        if (result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach((error) -> {
+                errores.put(error.getField(), "El campo".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()) );
+            });
+            model.addAttribute("errores", errores);
+            return "form";
+        }
+        model.addAttribute("usuario", usuario);
+        return "resultado";
+    }
+
+    /* @PostMapping("/form")
     public String procesarForm(Model model,
                                 @RequestParam String username,
                                 @RequestParam String password,
                                 @RequestParam String email) {
 
+        Usuario usuario = new Usuario(username, password, email);
         model.addAttribute("titulo", "Resultado del POST");
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
-        model.addAttribute("username", username);   
-    
+        model.addAttribute("usuario", usuario);
         return "resultado";
-    }
+    } */
 }
---
+++ src/main/java/com/chaldu/formularioweb/formularioweb/models/domain/Usuario.java
@@ -0,0 +1,40 @@
+package com.chaldu.formularioweb.formularioweb.models.domain;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class Usuario {
+
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+    @NotEmpty
+    private String email;
+
+
+    public Usuario(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
